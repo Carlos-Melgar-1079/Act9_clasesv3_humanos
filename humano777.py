b62789fbdff144105c770eebec341c513a9731ae
CarlosMelgar <164534523+Carlos-Melgar-1079@users.noreply.github.com>
2024-09-20 11:53:01 -0600

    Add files via upload

---
+++ humano777.py
@@ -0,0 +1,80 @@
+print("Act 9 clases humano")
+print("Carlos Esteban Melgar Oropeza:22308051281079")
+#zona de clases
+
+class Humano1079:
+    #zona de atributos dentro de la clase
+    #hombre
+    peda=0
+    cabello=0
+    estatura=0
+    tipotenis=0
+    dianacimiento=0
+    #mujer
+    colorcab=0
+    ojos=0
+    genero=0
+    dianacimiento2=0
+    #zona de funciones dentro de la clase
+    #----------------------Hombre-------------------------------------
+    def pistear1079(self,n):
+        print(f"{n} esta pisteando miller ufff....")
+    def juniroh21079(self, a):
+        print(f"{a} es su color de pelo")
+    def jordan1079(self, b):
+        print(f"{b} es su estatura es alto ufff..")
+    def jueves101079(self, c):
+        print(f"{c} son piratas sus tenis jajaja que jodido")
+    def carlitos1079(self, d):
+        print(f"{d} fue el dia que llego al mundo")
+        
+    #-----------------------------------mujer----------------------------------------------
+    def diasnublados1079(self,e):
+        print(f"{e} es su cabello")
+    def otroamor21079(self, f):
+        print(f"{f} que bonitos ojos")
+    def tresbotellasa1079(self, g):
+        print(f"es {g} Eunice")
+    def paris1079(self, h):
+        print(f"{h} fue el dia que llego al mundo")
+#zona de creacion de objetos
+Carlos=Humano1079()
+Eunice=Humano1079()
+
+#zona de usando objetos
+print("Resulta para Carlos")
+Carlos.peda="65%"
+Carlos.cabello="negro"
+Carlos.estatura=178
+Carlos.tipotenis="fuma"
+Carlos.dianacimiento=22
+print(f"porcentaje de pedes:{Carlos.peda}")
+Carlos.pistear1079("Carlos")
+print(f"el color de su pelo es :{Carlos.cabello}")
+Carlos.juniroh21079("negro")
+print(f"porcentaje de pedes:{Carlos.estatura}")
+Carlos.pistear1079("178")
+print(f"porcentaje de pedes:{Carlos.tipotenis}")
+Carlos.pistear1079("fuma")
+print(f"porcentaje de pedes:{Carlos.dianacimiento}")
+Carlos.pistear1079("22")
+
+print("--------------------------------------------mujer-------------------------------------------------------------")
+
+print("Resulta para Eunice")
+Eunice.colorcab="rojo"
+Eunice.ojos="cafes"
+Eunice.genero="mujer"
+Eunice.dianacimiento=6
+print(f"color de cabello:{Eunice.colorcab}")
+Eunice.pistear1079("rojo")
+print(f"el color de su pelo es :{Eunice.ojos}")
+Eunice.juniroh21079("cafes")
+print(f"porcentaje de pedes:{Eunice.genero}")
+Eunice.pistear1079("mujer")
+print(f"porcentaje de pedes:{Eunice.dianacimiento}")
+Eunice.pistear1079("6")
+
+
+
+
